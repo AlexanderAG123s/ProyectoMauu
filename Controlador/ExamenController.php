f1e20e6f6e66bd2d5e49d4a1fb8ee4ea4b8a716c
AlexanderAG123s <jjsmid4@gmail.com>
2024-10-02 22:38:32 -0600

    waos

--- Controlador/ExamenController.php
+++ Controlador/ExamenController.php
@@ -84,7 +84,7 @@ if ($_POST['funcion'] == 'generar_examen') {
    
        $pdf->SetFont('Arial', 'B', 12, '', true); // Add the fifth parameter as true to enable UTF-8
        $pdf->MultiCell(200, 10, $i.'.- '. utf8_decode($objeto->question), 0, 'L'); // Use MultiCell with a width of 0, which means the cell will take up the full width of the page
-       $pdf->Ln(1); // Add a small line break between questions
+       $pdf->Ln(0.5); // Add a small line break between questions
        $pdf->SetFont('Arial', '', 12, '', true); // Add the fifth parameter as true to enable UTF-8
        $pdf->MultiCell(0, 10, utf8_decode($option1.'.- '.$objeto->respuesta_f), 0, 'L');
        $pdf->Ln(0.5);
