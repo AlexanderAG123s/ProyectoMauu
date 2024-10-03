5e6a0fcc84834deb8bd2a66c4a3d309d51aaba1b
AlexanderAG123s <jjsmid4@gmail.com>
2024-10-02 23:58:16 -0600

    w

--- Modelo/Examen.php
+++ Modelo/Examen.php
@@ -29,14 +29,16 @@ include_once 'conexion.php';
                 $consulta=$_POST['consulta'];
                 $checkedDataIds=$_POST['checkedDataIds'];
                 $parcial = $_POST['parcial'];
-                $sql= "SELECT preguntas.id, preguntas.question, preguntas.respuesta_f, preguntas.respuesta_f2, preguntas.respuesta_c, temas.titulo as nombre_tema 
-                FROM preguntas
-                JOIN temas ON preguntas.tema = temas.id
-                WHERE temas.asignatura LIKE :consulta AND temas.parcial LIKE :parcial AND temas.id IN :temas
-                ORDER BY RAND()
-                LIMIT 25"; // adjust the limit to the desired number of random preguntas
+                $sql = "SELECT preguntas.id, preguntas.question, preguntas.respuesta_f, preguntas.respuesta_f2, preguntas.respuesta_c, temas.titulo as nombre_tema 
+        FROM preguntas
+        JOIN temas ON preguntas.tema = temas.id
+        WHERE temas.asignatura LIKE :consulta AND temas.parcial LIKE :parcial AND temas.id IN (";
+
+$sql .= implode(', ', $checkedDataIds);
+
+$sql .= ") ORDER BY RAND() LIMIT 25"; // adjust the limit to the desired number of random preguntas
                 $query = $this->acceso->prepare($sql);
-                $query->execute(array(':consulta' => "%$consulta%", ':parcial' => "%$parcial%", ':temas' => implode(',', $checkedDataIds)));
+                $query->execute(array(':consulta' => "%$consulta%", ':parcial' => "%$parcial%"));
                 $this->objetos=$query->fetchall();
                 return $this->objetos;
             } else {
