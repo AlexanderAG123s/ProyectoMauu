fdb9c980e1be039a0a632e10fab262bee9142565
AlexanderAG123s <jjsmid4@gmail.com>
2024-10-03 12:45:17 -0600

    w

--- Controlador/ExamenController.php
+++ Controlador/ExamenController.php
@@ -116,12 +116,9 @@ if (strlen($response3) > 18) {
    
    
    
-   $pdf->SetFont('Arial', 'B', 12, '', true); // Add the fifth parameter as true to enable UTF-8
-   $pdf->Ln(10);
-   $pdf->MultiCell(200, 10,'Hoja de Respuestas' , 0, 'C'); // Use MultiCell with a width of 0, which means the cell will take up the full width of the page
-   $pdf->Ln(0.2); // Add a small line break between questions
 
-   $pdf->Image('../img/waos.png', 0, 45, 210, 200);
+
+   $pdf->Image('../img/waos.png', 0, 25, 210, 200);
    
    $pdf->Output('examen.pdf', 'I'); // Save the PDF to a file on the server
    echo '../PDF/Output/'.$_POST['funcion'].'.pdf'; // Return the URL to the PDF file
