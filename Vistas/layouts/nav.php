dfd69f478e5d703fa17ba74b627f79f60b8ac584
AlexanderAG123s <jjsmid4@gmail.com>
2024-10-03 11:41:18 -0600

    waos

--- Vistas/layouts/nav.php
+++ Vistas/layouts/nav.php
@@ -34,12 +34,6 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
     </ul>
 
     <!-- Right navbar links -->
@@ -79,7 +73,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="../vistas/adm_inicio.php" class="brand-link">
       <img src="../img/logo.webp" alt="F" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Banco de Preguntas</span>
     </a>
